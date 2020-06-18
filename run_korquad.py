7b7953970a3547501c6514df657047db062e15da
Sunkyoung <sunk10926@gmail.com>
2020-06-18 19:16:07 +0900

    Update run_korquad.py

--- run_korquad.py
+++ run_korquad.py
@@ -12,7 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Run BERT on SQuAD 1.1 and SQuAD 2.0."""
+# """Run BERT on SQuAD 1.1 and SQuAD 2.0."""
+"""Run BERT on KorQuAD 2.1"""
 
 from __future__ import absolute_import
 from __future__ import division
